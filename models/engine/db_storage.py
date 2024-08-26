25fbc43df74c0640ef77378ad63532beabceb4e0
Edison pryce <Edisonpryce@gmail.com>
2024-08-26 02:45:13 +0000

    I made changes to the db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,18 +1,20 @@
 """ Using sqlalchemy to create database and tables
 """
 from uuid import uuid4
+from hashlib import md5
 from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
+
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     # Columns
-    user_id = Column(String(36), primary_key=True, nullable=False, default=lambda: str(uuid4()))
+    id = Column(String(36), primary_key=True, nullable=False, default=lambda: str(uuid4()))
     email = Column(String(46), nullable=False)
     password = Column(String(150), nullable=False)
     first_name = Column(String(14), nullable=True)
@@ -20,21 +22,35 @@ class User(Base):
     telephone = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.now(), nullable=True)
     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=True)
+    cart_item = relationship("Cart", backref="user")
+    order_item = relationship("Order", backref="user")
+
+    def __str__(self):
+        return f"<User {self.id}>"
 
 
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
 
 # Creation of the products table
 class Product(Base):
-    __tablename__ = 'product'
+    __tablename__ = 'products'
 
     id = Column(String(36), primary_key=True, nullable=False, default=lambda: str(uuid4()))
-    product_name = Column(String(128), nullable=False)
+    product_name = Column(String(36), nullable=False)
     current_price = Column(Integer, nullable=False)
     chosen_price = Column(Integer, nullable=False)
     in_stock = Column(Integer, nullable=False)
-    product_image = Column(String(128), nullable=True)
+    product_image = Column(String(36), nullable=True)
     created_at = Column(DateTime, default=datetime.now())
-
+    carts = relationship("Cart", backref="product")
+    orders = relationship("Order", backref="product")
+    
+    def __str__(self):
+        return f"<Product {self.id}>"
 
 
 # Making of the cart table
@@ -43,9 +59,12 @@ class Cart(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     quantity = Column(Integer, nullable=False)
+    customer_link = Column(String(36), ForeignKey('users.id'), nullable=False)
+    product_link = Column(String(36), ForeignKey('products.id'), nullable=False)
 
-    customer_link = Column(String(36), ForeignKey('user.id'), nullable=False)
-    product_link = Column(String(36), ForeignKey('product.id'), nullable=False)
+    def __str__(self):
+        return f"<Cart {self.id}>"
+
     
 class Order(Base):
     __tablename__ = 'order'
@@ -53,12 +72,13 @@ class Order(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     quantity = Column(Integer, nullable=False)
     price = Column(Float, nullable=False)
-    status = Column(String(100), nullable=False)
-    payment_id = Column(String(255), nullable=False)  # Adjusted length to a reasonable size
+    status = Column(String(60), nullable=False)
+    payment_id = Column(String(36), nullable=False) 
+    customer_link = Column(String(36), ForeignKey('users.id'), nullable=False)
+    product_link = Column(String(36), ForeignKey('products.id'), nullable=False)
 
-    customer_link = Column(String(36), ForeignKey('user.id'), nullable=False)
-    product_link = Column(String(36), ForeignKey('product.id'), nullable=False)
-
+    def __str__(self):
+        return f"<Order {self.id}>"
 
 
 
