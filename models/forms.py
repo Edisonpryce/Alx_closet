f50a475359082e138d43044f0c76526a079acb25
Edison Asare <104757179+Edisonpryce@users.noreply.github.com>
2024-08-27 07:34:00 +0000

    update on the form table

---
+++ models/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, FloatField, PasswordField, EmailField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired, length, NumberRange
+from flask_wtf.file import FileField, FileRequired
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), length(min=2)])
+    email = EmailField('Email', validators=[DataRequired()])
+    password1 = PasswordField('Password', validators=[DataRequired(), length(min=6)])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), length(min=6)])
+    submit = SubmitField('Create account')
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class PasswordChangeForm(FlaskForm):
+    current_password = PasswordField('Current Password', validators=[DataRequired(), length(min=6)])
+    new_password = PasswordField('New Password', validators=[DataRequired(), length(min=6)])
+    confirm_new_password = PasswordField('Confirm New Password', validators=[DataRequired(), length(min=6)])
+    change_password = SubmitField('Change Password')
