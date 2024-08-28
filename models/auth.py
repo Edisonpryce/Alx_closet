1381ca3a3feadcaf317c18e0023e5211533ccbfe
Edison pryce <Edisonpryce@gmail.com>
2024-08-28 10:59:41 +0000

    changes rectify on the ancho tags of the index.html

--- models/auth.py
+++ models/auth.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash, current_app, Blueprint
-from .forms import SignUpForm  # Import your form
+from .forms import SignUpForm, LoginForm  # Import your form
 from .tables import User
 import hashlib
 
@@ -7,13 +7,31 @@ auth = Blueprint('auth', __name__)
 
 
 
-@auth.route('/login', strict_slashes=False)
+@auth.route('/login', methods=['GET', 'POST'], strict_slashes=False)
+def login():
+    session = current_app.config['SESSION']
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        user = session.query(User).filter_by(email=email).first()
+
+        if user and user.verify_password(password):
+            flash('Login successful!', 'success')
+            return redirect(url_for('customer.dashboard'))
+        else:
+            flash('Invalid email or password', 'danger')
+
+    return render_template('login.html', form=form)
+
+
 @auth.route('/signup', methods=['GET', 'POST'], strict_slashes=False)
 def signup():
+    session = current_app.config['SESSION']
     form = SignUpForm()
     if form.validate_on_submit():
-        # Assuming you have a method to save data from the form to the database
-        session = current_app.config['SESSION'] 
         username = form.username.data
         email = form.email.data
         phone = form.phone.data
