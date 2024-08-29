46fa58842ff015568dc145253802d49112c927d8
jan-ice17 <judysjewel17@gmail.com>
2024-08-30 02:02:54 +0300

    Images for shop products added

--- static/js/script.js
+++ static/js/script.js
@@ -132,3 +132,50 @@ const scrollReveal = function () {
 scrollReveal();
 
 addEventOnElem(window, "scroll", scrollReveal);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+  const cartCountElement = document.getElementById('cart-count');
+  const modal = document.getElementById('impact-modal');
+  const closeModalButton = document.querySelector('.modal .close');
+  const impactMessage = document.getElementById('impact-message');
+  const addToCartModalButton = document.getElementById('add-to-cart-btn');
+  const cancelModalButton = document.getElementById('cancel-btn');
+  let cartCount = 0;
+
+  addToCartButtons.forEach((button, index) => {
+      button.addEventListener('click', function() {
+          const priceSlider = document.getElementById(`price-slider-${index + 1}`);
+          const selectedPrice = parseInt(priceSlider.value, 10);
+
+          if (selectedPrice > 100) {
+              impactMessage.innerHTML = `Woooow $${selectedPrice} 😱 Through your purchase this is going to help us support others💗💗`;
+          } else {
+              impactMessage.innerHTML = `Thank you for your purchase of $${selectedPrice}!`;
+          }
+
+          modal.style.display = "block";
+
+          addToCartModalButton.onclick = function() {
+              cartCount++;
+              cartCountElement.textContent = cartCount;
+              modal.style.display = "none";
+          };
+      });
+  });
+
+  closeModalButton.addEventListener('click', function() {
+      modal.style.display = "none";
+  });
+
+  cancelModalButton.addEventListener('click', function() {
+      modal.style.display = "none";
+  });
+
+  window.addEventListener('click', function(event) {
+      if (event.target === modal) {
+          modal.style.display = "none";
+      }
+  });
+});
