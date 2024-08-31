e4af41408428ce590b5eff53daf47cf647158c58
Edison pryce <Edisonpryce@gmail.com>
2024-08-31 22:06:40 +0000

    new changes to the client file

--- models/client.py
+++ models/client.py
@@ -14,7 +14,22 @@ customer = Blueprint('customer', __name__)
 def index():
     return render_template('/index.html')
 
+@customer.route('/product1', methods=['GET', 'POST'], strict_slashes=False)
+def product1():
+    return render_template('/product1.html')
+
+
+@customer.route('/product2', methods=['GET', 'POST'], strict_slashes=False)
+def product2():
+    return render_template('/product2.html')
+
 @customer.route('/home', methods=['GET', 'POST'], strict_slashes=False)
 @login_required
 def home():
     return render_template('/home.html', user=current_user.name)
+
+
+@customer.route('/profile', methods=['GET', 'POST'], strict_slashes=False)
+@login_required
+def profile():
+    return render_template('/profile.html', user=current_user.name)
