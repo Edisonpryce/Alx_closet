04afb9ceba5ee6766322b7278d17496be3c50df8
jan-ice17 <judysjewel17@gmail.com>
2024-08-31 20:51:26 +0300

    Linking product1 to index file in flask

--- models/client.py
+++ models/client.py
@@ -14,7 +14,12 @@ customer = Blueprint('customer', __name__)
 def index():
     return render_template('/index.html')
 
+@customer.route('/product1', methods=['GET', 'POST'], strict_slashes=False)
+def index():
+    return render_template('/product1.html')
+
 @customer.route('/home', methods=['GET', 'POST'], strict_slashes=False)
 @login_required
 def home():
     return render_template('/home.html', user=current_user.name)
+
