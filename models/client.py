8484547c582684ea8ac106d6fe22e4d316b02475
Edison pryce <Edisonpryce@gmail.com>
2024-09-01 08:07:07 +0000

    new feature added

--- models/client.py
+++ models/client.py
@@ -37,6 +37,7 @@ def home():
     return render_template('/home.html', user=current_user.name)
 
 
+@customer.route('/home/profile', methods=['GET', 'POST'], strict_slashes=False)
 @customer.route('/profile', methods=['GET', 'POST'], strict_slashes=False)
 @login_required
 def profile():
