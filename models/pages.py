fd37f1062f15cb09ee7d9ee39051bcd15f4683bf
Edison pryce <Edisonpryce@gmail.com>
2024-09-03 20:36:04 +0000

    new changes to the application

--- models/pages.py
+++ models/pages.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 
 page = Blueprint('pages', __name__)
 
-@page.route('/', strict_slashes=False)
+@page.route('/add_to_cart/<str:item_id>', strict_slashes=False)
 def home():
     return render_template("index.html")
