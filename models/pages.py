7e305e7d1ee1e6dad7e014ee6ca688b155ea5c1d
Edison pryce <Edisonpryce@gmail.com>
2024-09-03 22:08:31 +0000

    problem resolved

--- models/pages.py
+++ models/pages.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 
 page = Blueprint('pages', __name__)
 
-@page.route('/add_to_cart/<str:item_id>', strict_slashes=False)
+@page.route('/add_to_cart/<item_id>', strict_slashes=False)
 def home():
     return render_template("index.html")
